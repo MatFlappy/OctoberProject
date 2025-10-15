48c8014e829223b5183b8ff7dc2867175c41ec60
19mat <19mat@MATVEY>
2025-10-15 22:28:00 +0300

    Добавьте файлы проекта.

---
+++ Case_37/Registraition.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Case_37
+{
+    /// <summary>
+    /// Логика взаимодействия для Registraition.xaml
+    /// </summary>
+    public partial class Registraition : Window
+    {
+        public Registraition()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
