7f94c81008b1dca4605997245ee0eb880c1d9dc5
FlappYes <19mat19@mail.ru>
2025-10-20 08:25:30 +0300

    1

--- Case_37/App.xaml.cs
+++ Case_37/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Case_37.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace Case_37
     /// </summary>
     public partial class App : Application
     {
+        public static CaseEntities context = new CaseEntities();
     }
 }
---
+++ Case_37/View/Windows/Menu.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Case_37.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Case_37
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        //List<Game> _game = new List<Game>();
+        public Menu()
+        {
+           
+            InitializeComponent();
+            Lst.ItemsSource = App.context.Game.ToList();
+
+
+
+        }
+
+        //private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    Lst.ItemsSource = App.context.Game.ToList();
+        //}
+
+        //private void SteachTb_GotFocus(object sender, RoutedEventArgs e)
+        //{
+        //    if (SteachTb.Text == "Введите название")
+        //    {
+        //        SteachTb.Text = "";
+        //    }
+        //}
+
+        //private void SteachTb_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    if (SteachTb.Text == "")
+        //    {
+        //        SteachTb.Text = "Введите название";
+        //    }
+        //}
+
+        //private void Search()
+        //{
+        //    _game = App.context.Game.ToList();
+        //   _game = _game.Where(x => x.Name.ToLower().Contains(SteachTb.Text.ToLower())).ToList();
+            
+        //}
+
+        //private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        //{ 
+        //    Search();
+        //}
+    }
+}
